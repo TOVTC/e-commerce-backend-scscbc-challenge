8d5b1f5ac5342441178b7d173f3dca85bec7e4e2
TOVTC <veronica.tc.to@outlook.com>
2022-05-17 15:05:52 -0400

    added get .findAll() routes for category, index, and product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    order: [['id', 'ASC']],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'tags'
+      }
+    ]
+  })
+  .then(product => {res.json(product)})
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
